11404ab0c6edf7a8a282f411bad5efe73c87247b
diitaz93 <juan.sebastian.diaz.boada@ki.se>
2022-07-07 16:43:32 +0200

    MOre folrmal code + Improved the docstring of the python file + removed the format specification of the plot, making the plot be saved in the format the user specifies + Improved appearance of the code

--- src/pyVolcano.py
+++ src/pyVolcano.py
@@ -1,14 +1,47 @@
 #!/usr/bin/env python
 # coding: utf-8
+# =========================================================================== #
+# pyVolcano.py                                                                #
+# Author: Juan Sebastian Diaz Boada                                           #
+# Creation Date: 07/07/22                                                     #
+# =========================================================================== #
 
-# # pyVolcano
-# Volcano plot working over matplotlib, numpy and pandas
+""" Creates a volcano plot from a dataset and exports it.
+    Reads a dataset comming from a differential expression experiment, containing a list of genes
+    with their respective p-values and log2Fold values and generates a volcano plot. The plot is
+    exported in a file and format given by the user.
+
+    Parameters
+    ----------
+    in_path : string.
+        Path to the DE dataset. The dataset must contain the gene names, the p-values and the
+        log2Fold values in columns.
+    out_path : string.
+        Path to the file where the plot is going to be saved.
+    pval : float (optional).
+        P-value threshold that determines significance. Defaults to 0.01.
+    log2F : float (optional).
+        Log2Fold value threshold that determines significance. Defaults to 1.
+    pval_col : string (optional).
+        Name of the column corresponding to p-values in the DE dataset. Defaults to 'padj'.
+    log_col : string (optional).
+        Name of the column corresponding to log2Fold values in the DE dataset. Defaults to
+        'log2FoldChange'.
+    gene_col : string (optional).
+        Name of the column corresponding to gene names in the DE dataset. Defaults to 'gene'.
+    title : string (optional).
+        Title of the plot to be written on top of the plot. Defaults to 'Volcano plot'..
+    up_color : string (optional).
+        Color for the up-regulated genes. Defaults to 'green'.
+    down_color : string (optional).
+        Color for the down-regulated genes. Defaults to 'red'.
+
+"""
 
 import argparse
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-
 
 # Argparser definition
 parser = argparse.ArgumentParser(description="Parameters of volcano plot.")
@@ -18,8 +51,8 @@ parser.add_argument('--pval', type=float, default=0.01, help="P-value threshold 
 parser.add_argument('--log2F', type=float, default=1, help="Log2Fold threshold to determine significance. Defaults to 1.")
 parser.add_argument('-n','--n_names2show', type=int, default=0, help="Number of top gene names to show. Defaults to 0.")
 parser.add_argument('--pval_col', type=str, default='padj', help="Name of the column corresponding to p-values in the DE dataset. Defaults to 'padj'.")
-parser.add_argument('--log_col', type=str, default='log2FoldChange', help="Name of the column corresponding to log2 values in the DE dataset. Defaults to 'padj'.")
-parser.add_argument('--gene_col', type=str, default='gene', help="Name of the column corresponding to gene names in the DE dataset. Defaults to 'padj'.")
+parser.add_argument('--log_col', type=str, default='log2FoldChange', help="Name of the column corresponding to log2 values in the DE dataset. Defaults to 'log2FoldChange'.")
+parser.add_argument('--gene_col', type=str, default='gene', help="Name of the column corresponding to gene names in the DE dataset. Defaults to 'gene'.")
 parser.add_argument('--title', type=str, default='Volcano plot', help="Title of the plot. Defaults to 'Volcano plot'.")
 parser.add_argument('--up_color', type=str, default='green', help="Color for up-regulated genes. Defaults to 'green'.")
 parser.add_argument('--down_color', type=str, default='red', help="Color for down-regulated genes. Defaults to 'red'.")
@@ -39,9 +72,7 @@ down_color = args.down_color
 
 # Import dataset
 DF = pd.read_csv(in_file,sep='\t',index_col=0).reset_index(drop=True)
-
 # Sort DF properly
-
 DF.insert(2,'absLogF',np.absolute(DF.loc[:,'log2FoldChange']))
 DF = DF.sort_values(['padj','log2FoldChange'],ascending=[True,False]).reset_index(drop=True)
 
@@ -53,11 +84,9 @@ DF.loc[down,'color'] = down_color
 DF.loc[up,'color'] = up_color
 
 # Plot
-
 x = DF.loc[:,log_col].values
 y = -np.log10(DF.loc[:,pval_col].values)
 names = DF.loc[:,gene_col]
-
 fig,ax = plt.subplots(figsize=[8,8])
 ax.axhline(-np.log10(pval_thresh),color='gray',linestyle='--')
 ax.axvline(log_thresh,color='gray',linestyle='--')
@@ -69,6 +98,4 @@ for i in range(n_names2show):
 ax.set_ylabel(r'$-log_{10}(pval)$')
 ax.set_xlabel(r'$log_2FoldChange$')
 ax.set_title(title)
-plt.savefig(out_file,format='pdf')
-
-# In[ ]:
+plt.savefig(out_file)
